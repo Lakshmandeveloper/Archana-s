05ff0098a94f8db1c7e11ebb07914677544ad542
lakshmantetali <lakshmantetali@gmail.com>
2023-12-03 21:25:14 -0600

    first repo

---
+++ problem1.java
@@ -0,0 +1,23 @@
+public class problem1 {
+    public static void main(String[] args){
+        String S= "Python 3.0";
+        String word=" tutorial ";
+
+        for(int i=0;i<S.length();i++){
+
+            if(S.charAt(i)== ' '){
+
+                System.out.println(S.substring(0,i+1) +word +S.substring(i+1,S.length()));
+ }
+}
+
+
+
+            }
+
+
+            }
+
+
+
+
---
+++ problem2.java
@@ -0,0 +1,12 @@
+public class problem2 {
+    public static void main(String[] args){
+        int arr[]= {7,8,9,10,11,12,13,14,15};
+        int value= arr.length;
+        int array[]=new int[value];
+
+        int temp =arr[0];
+        int arr[0]= arr[value-1];
+
+    }
+
+}
